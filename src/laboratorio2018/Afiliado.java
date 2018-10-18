be6a24c5e101b71c780b7ff39feba35b0b8a11a4
maxi-avellaneda <43013518+maxi-avellaneda@users.noreply.github.com>
2018-10-18 20:00:36 -0300

    encapsulamos Afiliado
    encapsulamos Afiliado

--- src/laboratorio2018/Afiliado.java
+++ src/laboratorio2018/Afiliado.java
@@ -2,10 +2,66 @@
 package laboratorio2018;
 
 public class Afiliado {
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the apellido
+     */
+    public String getApellido() {
+        return apellido;
+    }
+
+    /**
+     * @param apellido the apellido to set
+     */
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    /**
+     * @return the documento
+     */
+    public Integer getDocumento() {
+        return documento;
+    }
+
+    /**
+     * @param documento the documento to set
+     */
+    public void setDocumento(Integer documento) {
+        this.documento = documento;
+    }
+
+    /**
+     * @return the familia
+     */
+    public Familia getFamilia() {
+        return familia;
+    }
+
+    /**
+     * @param familia the familia to set
+     */
+    public void setFamilia(Familia familia) {
+        this.familia = familia;
+    }
     
     private String nombre;
     private String apellido;
     private Integer documento;
-    public Familia familia;
+    private Familia familia;
             
 }
